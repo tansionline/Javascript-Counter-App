2d58cd147564a67f01d42d4a07cb16fe92d32985
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-11-26 15:15:29 +0300

    Counter App

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// variables 
+let counter = document.getElementById('counter')    
+let button = document.getElementById('button') 
+let number = 0
+
+// button and counter  
+button.addEventListener("click", () => { 
+   number++
+   counter.innerHTML = number
+
+}); 
+
+
+
+
+
+
